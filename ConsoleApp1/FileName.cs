7c5eddc494287b9c5a2680a67ff27450621ae17d
nicolas.navas <nicolas.navas@AUEA-C248>
2026-09-16 09:18:08 +0200

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/FileName.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class FileName
+    {
+        public FileName()
+        {
+            Console.WriteLine("Filename Construido");
+        }
+        public string Description = "Hello, World!";
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleApp1;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is a comment
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            FileName fileName = new FileName();
+            Console.WriteLine(fileName.Description);
+            //Console.WriteLine("Hello, World!");
+            //String? input = Console.ReadLine();
+            //Console.WriteLine(input);
+        }
+    }
+}
