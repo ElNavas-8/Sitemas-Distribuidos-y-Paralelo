209e05dd740acc099020f428ca87cd3afba4b802
ElNavas-8 <nicolas.navas@alumnos.uneatlantico.es>
2026-09-21 08:44:21 +0200

    feat: Encapsulacion en c#

---
+++ Proyecto21/Proyecto21_Sep/Proyecto2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Proyecto2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Vehiculo miVehiculo = new Vehiculo();
+            Vehiculo miVehiculo2 = new Vehiculo();
+
+            miVehiculo2.ModificarMarca(miVehiculo2, "Renault");
+
+            // String concatenada
+            Console.WriteLine("Mi auto es de marca " + miVehiculo.GetMarca());
+
+            // String con punteros
+            Console.WriteLine("Mi carrito es de marca {0}",miVehiculo2.GetMarca());
+        }
+    }
+}
---
+++ Proyecto21/Proyecto21_Sep/Proyecto2/Vehiculo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto2
+{
+    public class Vehiculo
+    {
+        public string Marca = "Seat";
+
+        public void ModificarMarca(Vehiculo vehiculo, string nuevaMarca)
+        {
+            vehiculo.Marca = nuevaMarca;
+        }
+        public string GetMarca()
+        {
+            return Marca;
+        }
+        public Vehiculo() {
+            
+        }
+    }
+}
