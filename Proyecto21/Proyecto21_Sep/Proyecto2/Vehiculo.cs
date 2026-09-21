1c29583de4dfeecf302d6454cc6521baac279e7b
ElNavas-8 <nicolas.navas@alumnos.uneatlantico.es>
2026-09-21 08:47:27 +0200

    chore: Reorganize repo

--- Proyecto21/Proyecto21_Sep/Proyecto2/Vehiculo.cs
+++ Proyecto21/Proyecto21_Sep/Proyecto2/Vehiculo.cs
@@ -8,18 +8,24 @@ namespace Proyecto2
 {
     public class Vehiculo
     {
+        // Atributo o Variable de instancia
         public string Marca = "Seat";
 
+        // Setter
         public void ModificarMarca(Vehiculo vehiculo, string nuevaMarca)
         {
             vehiculo.Marca = nuevaMarca;
         }
+
+        //Getter
         public string GetMarca()
         {
             return Marca;
         }
+
+        // Constructor
         public Vehiculo() {
-            
+            this.Marca = "Seat";
         }
     }
 }
